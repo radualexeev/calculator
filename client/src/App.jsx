28b9b55eca7db60e29991a2da785caef3f8a3897
Radu Alexeev <1radualexeev1@gmail.com>
2024-04-22 17:19:51 +0300

    small change in structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
+export default function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       Calculator App
     </div>
   );
-}
-
-export default App;
+};
