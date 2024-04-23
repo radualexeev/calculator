88d56b575b57082e2669a640d6a4173af9a6745f
Radu Alexeev <1radualexeev1@gmail.com>
2024-04-23 09:17:36 +0300

    add value to buttons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,24 +7,24 @@ export default function App() {
   return (
     <div className="w-screen h-screen bg-slate-400 flex justify-center items-center">
       <Container>
-        <Button />
+        <Button value={"C"}/>
         <Display />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+        <Button value={"7"}/>
+        <Button value={"8"}/>
+        <Button value={"9"}/>
+        <Button value={"+"}/>
+        <Button value={"4"}/>
+        <Button value={"5"}/>
+        <Button value={"6"}/>
+        <Button value={"-"}/>
+        <Button value={"1"}/>
+        <Button value={"2"}/>
+        <Button value={"3"}/>
+        <Button value={"/"}/>
+        <Button value={"0"}/>
+        <Button value={"."}/>
+        <Button value={"="}/>
+        <Button value={"x"}/>
       </Container>
     </div>
   );
