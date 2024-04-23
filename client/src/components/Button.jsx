877213669524c102f423ebe156a3bc968f42aa09
Radu Alexeev <1radualexeev1@gmail.com>
2024-04-23 10:39:00 +0300

    add shadow

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button( {value, color, background, border} ) {
     console.log(background)
     return (
-        <div className={`w-button h-button ${background} rounded-md border-b-8 ${border} flex justify-center items-center text-3xl ${color} cursor-pointer`}>
+        <div className={`w-button h-button ${background} rounded-md border-b-8 ${border} flex justify-center items-center text-3xl ${color} cursor-pointer hover:shadow-inner hover:shadow-[#79A6BA]`}>
             {value}
         </div>
     );
