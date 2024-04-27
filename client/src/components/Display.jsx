0ae7ceee9dd11f68c49be3ad80a2090510b6a12b
Radu Alexeev <1radualexeev1@gmail.com>
2024-04-27 19:06:09 +0300

    remove inner style

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -1,7 +1,5 @@
-export default function Display( {data} ) {
+export default function Display() {
     return (
-        <div className="text-white bg-[#7DA8BA] w-display h-display py-2 px-4 text-4xl rounded-md border-t-8 border-t-[#658695] text-right col-span-3">
-            {data}
-        </div> 
+        <input className="display text-white bg-[#7DA8BA] w-display h-display py-2 px-4 text-4xl rounded-md border-t-8 border-t-[#658695] text-right col-span-3 outline-none cursor-context-menu caret-transparent" />
     );
 };
