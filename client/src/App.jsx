10cc269286f144b58bce384d46012017ce8b2141
Radu Alexeev <1radualexeev1@gmail.com>
2024-04-29 13:38:58 +0300

    add calculate button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Container from "./components/Container";
 import Display from "./components/Display";
 import Button from "./components/Button";
 import ResetButton from "./components/ResetButton";
+import CalculateButton from "./components/CalculateButton";
 
 export default function App() {
   const [displayData, setDisplayData] = useState("0")
@@ -44,7 +45,7 @@ export default function App() {
         <Button value={"/"} background={"bg-[#FFF0F5]"} color={"text-[#79A6BA]"} border={"border-[#79A6BA]"} onClick={handleClick}/>
         <Button value={"0"} background={"bg-[#FFFFFF]"} color={"text-[#79A6BA]"} border={"border-[#79A6BA]"} onClick={handleClick}/>
         <Button value={"."} background={"bg-[#FFFFFF]"} color={"text-[#79A6BA]"} border={"border-[#79A6BA]"} onClick={handleClick}/>
-        <Button value={"="} background={"bg-[#F1FF98]"} color={"text-[#79A6BA]"} border={"border-[#9DA858]"} onClick={handleClick}/>
+        <CalculateButton />
         <Button value={"*"} background={"bg-[#FFF0F5]"} color={"text-[#79A6BA]"} border={"border-[#79A6BA]"} onClick={handleClick}/>
       </Container>
     </div>
---
+++ client/src/components/CalculateButton.jsx
@@ -0,0 +1,7 @@
+export default function CalculateButton() {
+    return (
+        <div className={`w-button h-button bg-[#F1FF98] rounded-md border-b-8 border-[#9DA858] flex justify-center items-center text-3xl text-[#79A6BA] cursor-pointer hover:shadow-inner hover:shadow-[#79A6BA] select-none`}>
+            {"="}
+        </div>
+    );
+};
