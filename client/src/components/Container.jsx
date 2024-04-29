3120ddbce27b68246bc562e306bf390f03f07e65
Radu Alexeev <1radualexeev1@gmail.com>
2024-04-29 23:38:46 +0300

    clear up

--- client/src/components/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container( {children} ) {
-    return (
-        <div className="w-calculator h-calculator p-4 bg-[#95D0EA] rounded-xl border-b-8 border-[#199EE1] grid grid-cols-4 gap-2 place-content-between place-items-center">
-            {children}
-        </div>
-    );
-};
